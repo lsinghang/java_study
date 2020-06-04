f58dde8465617f39878a0da6adbabe8eb4bb3527
lsinghang <845423266@qq.com>
2020-06-04 23:49:29 +0800

    面向对象 和 this 构造函数

---
+++ src/com/imooc/faceTo/Cat.java
@@ -0,0 +1,24 @@
+package com.imooc.faceTo;
+
+public class Cat {
+    String name;
+    int age;
+
+
+    public Cat() {
+        System.out.println("我是构造方法");
+    }
+
+    public Cat(String name, int age) {
+//        name = name;
+        this(); //  第一行 可以调用同一个类可以调用构造函数
+        this.name = name;
+        this.age = age;
+        this.introduce();
+        introduce();
+    }
+
+    public void introduce() {
+        System.out.println("我的名字：" + name + ' ' + "今年" + age);
+    }
+}
---
+++ src/com/imooc/faceTo/CatTest.java
@@ -0,0 +1,8 @@
+package com.imooc.faceTo;
+
+public class CatTest {
+    public static void main(String[] args) {
+        Cat one = new Cat("小明",10);
+//        one.introduce();
+    }
+}
